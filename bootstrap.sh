953502d8095ffe3b6f793f8c57f43f2d759bec1b
Stuart Wallace <stuart.wallace@genesis.global>
2018-09-27 14:20:43 +0100

    Switch bootstrap to be Fedora/CentOS compatible. Pull dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,16 @@
 # Minimal configuration required to bootstrap a new machine
 ###
 
-sudo apt install git
-sudo apt install ansible
+sudo dnf install git
+sudo dnf install ansible
 
 if [ ! -e ~/.ssh/id_rsa_github ]; then
     echo "No github SSH key found"
     ssh-keygen -f ~/.ssh/id_rsa_github -N ""
     echo "Copy this into Github:"
     cat ~/.ssh/id_rsa_github.pub
+    echo "Press a key to continue..."
+    read -n 1 -s
 fi
 
 if [ ! -e ~/.ssh/config ]; then
@@ -21,4 +23,10 @@ Host github.com
     IdentityFile ~/.ssh/id_rsa_github
 EOF
 fi
+
+mkdir -p ~/Projects
+
+cd ~/Projects
+
+git clone git@github.com:yoink00/dotfiles_devops.git
 
