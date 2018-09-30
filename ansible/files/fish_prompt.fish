a48e9487f573e24db535e235cb158bb09b2b70d4
Stuart Wallace <stuart@wapbot.co.uk>
2018-09-30 17:50:54 +0100

    Tweak the prompt

--- ansible/files/fish_prompt.fish
+++ ansible/files/fish_prompt.fish
@@ -38,8 +38,8 @@ function fish_prompt --description 'Write out the prompt'
     set_color -b black
     printf '%s%s%s%s%s%s%s%s%s%s%s%s%s' (set_color -o white) '❰' (set_color green) $USER (set_color white) '❙' (set_color yellow) (echo $PWD | sed -e "s|^$HOME|~|") (set_color white) $git_info (set_color white) '❱' (set_color white)
     if test $laststatus -eq 0
-        printf "%s✔%s≻%s " (set_color -o green) (set_color white) (set_color normal)
+        printf "\n%s✔%s$%s " (set_color -o green) (set_color white) (set_color normal)
     else
-        printf "%s✘%s≻%s " (set_color -o red) (set_color white) (set_color normal)
+        printf "\n%s✘%s$%s " (set_color -o red) (set_color white) (set_color normal)
     end
 end
